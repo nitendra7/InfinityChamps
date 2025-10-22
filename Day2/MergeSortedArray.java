9075d7fbda011b807fe0871ef97a0c693d52cabd
nitendra7 <nitendra8077@gmail.com>
2025-10-22 10:39:26 +0530

    Home Assignment2

---
+++ Day2/MergeSortedArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    if(n==0) return;
+    if(m==0){
+        for(int i=0;i<n;i++) nums1[i]=nums2[i];
+        return;
+    }
+    for(int i=0;i<n; i++){
+        nums1[i+m]=nums1[i+m]+nums2[i];
+       }
+       Arrays.sort(nums1);
+    }
+}
